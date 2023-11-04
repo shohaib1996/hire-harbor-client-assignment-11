8b297345cfbc2e07e239a18a7218275e2b44b863
shohaib1996 <khanshohaibhossain@gmail.com>
2023-11-04 23:43:01 +0600

    Navbar done

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -10,7 +10,7 @@ import { AuthContext } from "../../AuthProvider/AuthProvider";
 
 const Login = () => {
     const navigate = useNavigate()
-    const {signInUser} = useContext(AuthContext)
+    const {signInUser, signInWithGoogle} = useContext(AuthContext)
     const handleLogin = (e) => {
        
         
@@ -38,6 +38,18 @@ const Login = () => {
     
             })
             .catch(error => console.error(error))  
+}
+const handleSocial = (media) => {
+    media()
+        .then(result => {
+            console.log(result.user)
+            toast.success("Login Successfully")
+            navigate(location?.state ? location.state : "/")
+
+        })
+        .catch(error => {
+            console.error(error);
+        })
 }
     return (
 
@@ -75,7 +87,7 @@ const Login = () => {
                                     Or
                                 </div>
                                 <div className="space-y-5">
-                                    <button type="button" className="w-full py-3 px-4 inline-flex justify-center items-center gap-2 rounded-md border font-medium bg-white text-gray-700 shadow-sm align-middle hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-white focus:ring-blue-600 transition-all text-sm dark:bg-gray-800 dark:hover:bg-slate-800 dark:border-gray-700 dark:text-gray-400 dark:hover:text-white dark:focus:ring-offset-gray-800 hover:scale-110 hover:border-2 hover:border-green-600 mb-2 mt-2">
+                                    <button onClick={() => handleSocial(signInWithGoogle)} type="button" className="w-full py-3 px-4 inline-flex justify-center items-center gap-2 rounded-md border font-medium bg-white text-gray-700 shadow-sm align-middle hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-white focus:ring-blue-600 transition-all text-sm dark:bg-gray-800 dark:hover:bg-slate-800 dark:border-gray-700 dark:text-gray-400 dark:hover:text-white dark:focus:ring-offset-gray-800 hover:scale-110 hover:border-2 hover:border-green-600 mb-2 mt-2">
                                         <svg className="w-4 h-auto" width="46" height="47" viewBox="0 0 46 47" fill="none">
                                             <path d="M46 24.0287C46 22.09 45.8533 20.68 45.5013 19.2112H23.4694V27.9356H36.4069C36.1429 30.1094 34.7347 33.37 31.5957 35.5731L31.5663 35.8669L38.5191 41.2719L38.9885 41.3306C43.4477 37.2181 46 31.1669 46 24.0287Z" fill="#4285F4" />
                                             <path d="M23.4694 47C29.8061 47 35.1161 44.9144 39.0179 41.3012L31.625 35.5437C29.6301 36.9244 26.9898 37.8937 23.4987 37.8937C17.2793 37.8937 12.0281 33.7812 10.1505 28.1412L9.88649 28.1706L2.61097 33.7812L2.52296 34.0456C6.36608 41.7125 14.287 47 23.4694 47Z" fill="#34A853" />
